d2686e33143324b96ddd2ac7c858f56fe71de561
williamle92 <william1992le@gmail.com>
2021-10-24 01:54:43 -0700

    did all routes besides delete

--- app/routes.py
+++ app/routes.py
@@ -26,7 +26,7 @@ def create_user():
     db.session.commit()
     response = jsonify(user.to_dict())
     response.status_code = 201
-    # response.headers['Location'] = url_for('api.get_user', id=user.id)
+    response.headers['Location'] = url_for('api.get_user', id=user.id)
     return response
 
 
--- migrations/versions/b1a16eb1429b_.py
+++ migrations/versions/b1a16eb1429b_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: c13add14fbc5
+Revision ID: b1a16eb1429b
 Revises: 
-Create Date: 2021-10-24 01:18:15.694205
+Create Date: 2021-10-24 01:49:00.071268
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'c13add14fbc5'
+revision = 'b1a16eb1429b'
 down_revision = None
 branch_labels = None
 depends_on = None
